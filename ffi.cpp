d9bee96cb6e51634d6e5de252daf742b9a594cf1
Keith A. Lewis <kal@kalx.net>
2020-01-25 23:47:10 -0500

    sync

--- ffi.cpp
+++ ffi.cpp
@@ -20,6 +20,8 @@
 using namespace ffi;
 
 std::map<std::string, ffi::dl> library; // library map;
+std::map<std::string, std::pair<void*,ffi::cif>> dictionary;
+ffi::stack stack;
 
 // convert -lxx to libxx.so
 std::string lib_name(const token_view& v)
@@ -36,31 +38,46 @@ std::string lib_name(const token_view& v)
 
 void load_library(const token_list& v)
 {
-	assert(v.size() > 2);
-	std::string lib = lib_name(v[0]);
-	auto i = library.find(lib);
+	// use -lxxx for key. all symbols go in the same dictionary
+	auto i = library.find(make_string(v[0]));
 	if (i == library.end()) {
-		ffi::dl dl(make_string(v[1]).c_str());
+		std::string lib = lib_name(v[0]);
+		ffi::dl dl(lib.c_str()); // LAZY???
+		assert (dl);
 		auto ib = library.insert(std::pair(lib, dl));
 		assert (ib.second);
 		i = ib.first;
 	}
-
+	auto func = make_string(v[1]);
+	void* sym = i->second.sym(func.c_str());
+	assert (sym);
+	ffi_type* ret = type_map[make_string(v[2])];
+	std::vector<ffi_type*> arg;
+	for (size_t i = 3; i < v.size(); ++i) {
+		arg.push_back(type_map[make_string(v[i])]);
+	}
+	ffi::cif cif(arg, ret);
+	dictionary[func] = std::pair(sym, cif);
 }
 
 void evaluate(const char* b, const char* e)
 {
-	std::vector<token_view> v = ffi::parse_line(b, e);
+	token_list v = ffi::parse_line(b, e);
 
 	for (const auto& t : v) {
 		std::cout << ">" << std::string_view(t.first, t.second - t.first) << "<" << std::endl;
 	}
 
-	if (v.size() > 0) {
-		if (0 == strncmp(v[0].first, "-l", 2)) {
-			load_library(v);
+	if (0 == strncmp(v[0].first, "-l", 2)) {
+		load_library(v);
+	}
+	else {
+		const auto i = dictionary.find(make_string(v[0]));
+		if (i != dictionary.end()) {
+			
 		}
 	}
+	// else if v[0] in dictionary
 }
 
 int main(int ac, char* av[])
